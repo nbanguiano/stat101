8cc02e45dbe0ec6f5b2dacc6109bdec4464a6101
Nicolas Anguiano <nicolas.anguiano@gmail.com>
2014-05-12 11:43:44 +0200

    added corr function for assignment 2

---
+++ R/specdata/corr.R
@@ -0,0 +1,24 @@
+corr <- function(d, thr = 0) {
+     
+     wd <- paste("D:/Users/nanguiano/Documents/Personal/Coursera/stat101/R/", d, sep = "")
+     setwd(wd)
+     
+     ids <- 1:length(list.files(pattern = ".csv"))    
+     gobs <- complete(d, ids)
+     correlations <- NA
+  
+     for (n in ids) {
+          if (gobs[n, "nobs"] > thr) {
+               thrData <- read.csv(list.files()[n])
+               goodobs <- complete.cases(thrData)
+               ts <- thrData$sulfate[goodobs]
+               tn <- thrData$nitrate[goodobs]
+               polcorr <- cor(ts, tn)
+               correlations <- c(correlations, polcorr)
+          }
+     }
+     
+     if (length(correlations) == 1) { correlations <- numeric(0) }
+     
+     return(correlations[!is.na(correlations)])
+}
